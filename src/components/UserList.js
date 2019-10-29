3895f4feddd4fec845ec69460ffef5d011233fcb
Steve Golden <sgolden2525@outlook.com>
2019-10-28 23:57:08 -0500

    Show no games user list
    Added show no games user list functionality

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-function UserList(props) {
-  console.log(props.showUsers)
-  return (
-    <React.Fragment>
-      <div>UserList</div>
-      <ul>
-        {props.showUsers.map((map) => {
+class UserList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.showGames = this.showGames.bind(this);
+  }
+  state = {
+    isGamePlayedChecked: false
+  }
+
+  showGames() {
+    this.setState((prevState) => ({
+      isGamePlayedChecked: !prevState.isGamePlayedChecked
+    }))
+    console.log(this.state.isGamePlayedChecked)
+  }
+
+   
+  render() {
+    let usersList;
+    if (this.state.isGamePlayedChecked === false) {
+      usersList = <ul>
+        {this.props.showUsers.map((map) => {
           return <li key={map.userName}>{`${map.userName} has played ${map.gamesPlayed} games.`}</li>
         })}
       </ul>
-    </React.Fragment>
-  )
+    } else {
+      usersList = <ul>
+        {this.props.showUsers.map((map) => {
+          return <li key={map.userName}>{`${map.userName}`}</li>
+        })}
+      </ul>
+    }
+
+    return (
+      <React.Fragment>
+        <label htmlFor="showgamesplayed">Show games played</label>
+        <input id="showgamesplayed" type="checkbox" onClick={this.showGames} />
+        <div>UserList</div>
+        {usersList}
+      </React.Fragment>
+    )
+  }
 }
 
 
